8b3eac95f9ccc629e864617e91e8bcf58ea488a4
Alexei Drummond <alexei.drummond@gmail.com>
2026-04-25 10:44:45 +1200

    Register MutableAlignment + phylonco services in ExchangeGibbsOperator test
    Without these addServices calls the BEAST class loader can't resolve
    mutablealignment.MutableAlignment and the test fails at XML parse time
    before reaching the MCMC. After the fix, the test runs and reproduces a
    likelihood mismatch during pre-burnin (sample 0).

--- phylonco-beast/src/test/java/phylonco/beast/evolution/readcountmodel/ExcangeGibbsOperatorTest.java
+++ phylonco-beast/src/test/java/phylonco/beast/evolution/readcountmodel/ExcangeGibbsOperatorTest.java
@@ -1,31 +1,24 @@
 package phylonco.beast.evolution.readcountmodel;
 
-import beast.base.core.Function;
-import beast.base.inference.MCMC;
-import beast.base.inference.Operator;
-import beast.base.inference.State;
-import beast.base.inference.distribution.Dirichlet;
-import beast.base.inference.distribution.ParametricDistribution;
-import beast.base.inference.distribution.Prior;
-import beast.base.inference.parameter.RealParameter;
-import beast.base.util.Randomizer;
 import beastfx.app.beast.BeastMCMC;
-import org.apache.commons.math3.stat.StatUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import static beast.pkgmgmt.BEASTClassLoader.addServices;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExcangeGibbsOperatorTest {
+
+    @BeforeEach
+    public void setUp() {
+        addServices("version.xml");
+        addServices(Paths.get("lib", "MutableAlignment.version.xml").toString());
+    }
 
     @Test
     public void test() throws Exception {
